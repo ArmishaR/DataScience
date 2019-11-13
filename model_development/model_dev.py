f8dad0e2d3ad50eba102995ffe4c5c743b0a4b59
prioleaud <dprioleau@ufl.edu>
2019-11-13 12:03:43 -0500

    outputting average rslme for each model for cv and test set

--- model_development/model_dev.py
+++ model_development/model_dev.py
@@ -10,7 +10,7 @@ from sklearn import tree
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error, mean_squared_log_error
 
 df = pd.read_csv('training_data_wImpute_threshold_0.1.csv')
@@ -22,30 +22,67 @@ data = df.drop(columns=['meter_reading'])
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.3, random_state=42)
 
-reg_tree = tree.DecisionTreeRegressor()
-reg_tree = reg_tree.fit(X_train, y_train)
-predictions = reg_tree.predict(X_test)
+skf = StratifiedKFold(n_splits=10,shuffle=False,random=42)
 
-#print(y_test)
-#print(predictions)
+rslme_dt = []
+rslme_linear = []
+rslme_knn = []
+rslme_nn = []
+
+for train_index, test_index in skf.split(X_train, y_train):
+    
+    X_sub_train, X_valid = X_train[train_index], X_train[test_index]
+    y_sub_train, y_valid = y_train[train_index], y_train[test_index]
+    
+    reg_tree = tree.DecisionTreeRegressor(criterion='mse')
+    reg_tree = reg_tree.fit(X_sub_train, y_sub_train)
+    predictions = reg_tree.predict(X_valid)
+
+    rslme_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))    
+    #print(predictions)
+    #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    
+    # Note: returns negative values which causes error when calculating RSMLE
+    linear_reg = LinearRegression(fit_intercept=False).fit(X_sub_train,y_sub_train)
+    predictions = linear_reg.predict(X_valid)
+    
+    rslme_linear.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    #print(predictions)
+    #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    
+    knn_reg = KNeighborsRegressor(n_neighbors=2,metric='manhattan').fit(X_train,y_train)
+    predictions = knn_reg.predict(X_test)
+    
+    rslme_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    #print(predictions)
+    #print(np.sqrt(mean_squared_log_error(y_test,predictions)))
+    
+    nn = MLPRegressor(activation='logistic').fit(X_sub_train,y_sub_train)
+    predictions = nn.predict(X_valid)
+    
+    rslme_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    #print(predictions)
+    #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+
+
+""" Model Predictions of Validation Set"""
+
+
+
+""" Model Predictions of Test Set """
+
+# Regression Tree Results
+predictions = reg_tree.predict(X_test)  
 print(np.sqrt(mean_squared_log_error(y_test,predictions)))
 
-# Note: returns negative values which causes error when calculating RSMLE
-linear_reg = LinearRegression(fit_intercept=False).fit(X_train,y_train)
+# linear Regression Results
 predictions = linear_reg.predict(X_test)
+print(np.sqrt(mean_squared_log_error(y_test,predictions)))
 
-#print(predictions)
-#print(np.sqrt(mean_squared_log_error(y_test,predictions)))
-
-knn_reg = KNeighborsRegressor(n_neighbors=3,metric='euclidean').fit(X_train,y_train)
+# KNN Results
 predictions = knn_reg.predict(X_test)
-
-print(predictions)
 print(np.sqrt(mean_squared_log_error(y_test,predictions)))
 
-nn = MLPRegressor(activation='logistic').fit(X_train,y_train)
+# Multi Layer Perceptron Results
 predictions = nn.predict(X_test)
-
-print(predictions)
 print(np.sqrt(mean_squared_log_error(y_test,predictions)))
-
