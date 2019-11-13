c7f5db997a5ad05c9af51e3d20bdef774e19a784
prioleaud <dprioleau@ufl.edu>
2019-11-13 13:03:59 -0500

    updated script to fix bugs

--- model_development/model_dev.py
+++ model_development/model_dev.py
@@ -10,7 +10,7 @@ from sklearn import tree
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, mean_squared_log_error
 
 df = pd.read_csv('training_data_wImpute_threshold_0.1.csv')
@@ -22,23 +22,23 @@ data = df.drop(columns=['meter_reading'])
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.3, random_state=42)
 
-skf = StratifiedKFold(n_splits=10,shuffle=False,random=42)
+skf = KFold(n_splits=10,shuffle=True,random_state=42)
 
-rslme_dt = []
-rslme_linear = []
-rslme_knn = []
-rslme_nn = []
+rmsle_dt = []
+rmsle_linear = []
+rmsle_knn = []
+rmsle_nn = []
 
-for train_index, test_index in skf.split(X_train, y_train):
+for train_index, test_index in skf.split(X_train):
     
-    X_sub_train, X_valid = X_train[train_index], X_train[test_index]
-    y_sub_train, y_valid = y_train[train_index], y_train[test_index]
+    X_sub_train, X_valid = X_train.iloc[train_index], X_train.iloc[test_index]
+    y_sub_train, y_valid = y_train.iloc[train_index], y_train.iloc[test_index]
     
     reg_tree = tree.DecisionTreeRegressor(criterion='mse')
     reg_tree = reg_tree.fit(X_sub_train, y_sub_train)
     predictions = reg_tree.predict(X_valid)
 
-    rslme_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))    
+    rmsle_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))    
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     
@@ -46,21 +46,21 @@ for train_index, test_index in skf.split(X_train, y_train):
     linear_reg = LinearRegression(fit_intercept=False).fit(X_sub_train,y_sub_train)
     predictions = linear_reg.predict(X_valid)
     
-    rslme_linear.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    #rslme_linear.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     
-    knn_reg = KNeighborsRegressor(n_neighbors=2,metric='manhattan').fit(X_train,y_train)
-    predictions = knn_reg.predict(X_test)
+    knn_reg = KNeighborsRegressor(n_neighbors=2,metric='manhattan').fit(X_sub_train,y_sub_train)
+    predictions = knn_reg.predict(X_valid)
     
-    rslme_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    rmsle_knn.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_test,predictions)))
     
     nn = MLPRegressor(activation='logistic').fit(X_sub_train,y_sub_train)
     predictions = nn.predict(X_valid)
     
-    rslme_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    rmsle_nn.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
 
@@ -68,6 +68,9 @@ for train_index, test_index in skf.split(X_train, y_train):
 """ Model Predictions of Validation Set"""
 
 
+print('mean RMSLE = ',np.mean(rmsle_dt))
+print('mean RMSLE = ',np.mean(rmsle_knn))
+print('mean RMSLE = ',np.mean(rmsle_nn))
 
 """ Model Predictions of Test Set """
 
