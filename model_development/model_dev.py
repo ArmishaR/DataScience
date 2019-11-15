814b098b4421b7fbc8d22961a837b6dfdf47ac42
edrobina <edrobina@ufl.edu>
2019-11-15 11:18:35 -0500

    edited model_dev.py to add new evaluation metrics

--- model_development/model_dev.py
+++ model_development/model_dev.py
@@ -3,7 +3,7 @@
 
 """
 
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import tree
@@ -11,7 +11,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import KFold
-from sklearn.metrics import mean_squared_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error, mean_squared_log_error, mean_absolute_error, r2_score
 
 df = pd.read_csv('training_data_wImpute_threshold_0.1.csv')
 
@@ -25,61 +25,81 @@ X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.
 skf = KFold(n_splits=10,shuffle=True,random_state=42)
 
 rmsle_dt = []
+r2_dt = []
+adj_r2_dt = []
+mae_dt = []
+
 rmsle_linear = []
 rmsle_knn = []
 rmsle_nn = []
 
+print("start")
+
 for train_index, test_index in skf.split(X_train):
-    
+    print("doing kfold")
+
     X_sub_train, X_valid = X_train.iloc[train_index], X_train.iloc[test_index]
     y_sub_train, y_valid = y_train.iloc[train_index], y_train.iloc[test_index]
-    
+
     reg_tree = tree.DecisionTreeRegressor(criterion='mse')
     reg_tree = reg_tree.fit(X_sub_train, y_sub_train)
     predictions = reg_tree.predict(X_valid)
 
-    rmsle_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))    
+    rmsle_dt.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+    mae_dt.append(mean_absolute_error(y_valid, predictions))
+    r2 = r2_score(y_valid, predictions, multioutput='variance_weighted')
+    print("r2")
+    print(r2)
+    print("mae")
+    print(mean_absolute_error(y_valid, predictions))
+    r2_dt.append(r2)
+    adj_r2 = 1 - (1 - r2)*((len(y_valid)-1)/(len(y_valid)-len(X_valid.columns) - 1))
+    adj_r2_dt.append(adj_r2)
+
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
-    
+
     # Note: returns negative values which causes error when calculating RSMLE
-    linear_reg = LinearRegression(fit_intercept=False).fit(X_sub_train,y_sub_train)
+    '''linear_reg = LinearRegression(fit_intercept=False).fit(X_sub_train,y_sub_train)
     predictions = linear_reg.predict(X_valid)
-    
+
     #rslme_linear.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
-    
+
     knn_reg = KNeighborsRegressor(n_neighbors=2,metric='manhattan').fit(X_sub_train,y_sub_train)
     predictions = knn_reg.predict(X_valid)
-    
+
     rmsle_knn.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_test,predictions)))
-    
+
     nn = MLPRegressor(activation='logistic').fit(X_sub_train,y_sub_train)
     predictions = nn.predict(X_valid)
-    
-    rmsle_nn.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
+
+    rmsle_nn.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))'''
     #print(predictions)
     #print(np.sqrt(mean_squared_log_error(y_valid,predictions)))
 
 
 """ Model Predictions of Validation Set"""
 
-
 print('mean RMSLE = ',np.mean(rmsle_dt))
+print('mean R^2 = ',np.mean(r2_dt))
+print('mean Adj R^2 = ',np.mean(adj_r2_dt))
+print('mean MAE = ',np.mean(mae_dt))
+'''print('mean RMSLE = ',np.mean(rslme_linear))
 print('mean RMSLE = ',np.mean(rmsle_knn))
-print('mean RMSLE = ',np.mean(rmsle_nn))
+print('mean RMSLE = ',np.mean(rmsle_nn))'''
 
 """ Model Predictions of Test Set """
 
 # Regression Tree Results
-predictions = reg_tree.predict(X_test)  
+predictions = reg_tree.predict(X_test)
 print(np.sqrt(mean_squared_log_error(y_test,predictions)))
 
 # linear Regression Results
-predictions = linear_reg.predict(X_test)
+'''predictions = linear_reg.predict(X_test)
 print(np.sqrt(mean_squared_log_error(y_test,predictions)))
 
 # KNN Results
@@ -88,4 +108,4 @@ print(np.sqrt(mean_squared_log_error(y_test,predictions)))
 
 # Multi Layer Perceptron Results
 predictions = nn.predict(X_test)
-print(np.sqrt(mean_squared_log_error(y_test,predictions)))
+print(np.sqrt(mean_squared_log_error(y_test,predictions)))'''
