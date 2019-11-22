0fdbcfa05af236081e21f15765e016ddf89867b8
edrobina <edrobina@ufl.edu>
2019-11-22 14:24:39 -0500

    updated evaluations for new parameters

--- model_development/model_dev.py
+++ model_development/model_dev.py
@@ -14,7 +14,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, mean_squared_log_error, mean_absolute_error, r2_score
-#from yellowbrick.regressor import ResidualsPlot, PredictionError, CooksDistance, AlphaSelection
+from yellowbrick.regressor import ResidualsPlot, PredictionError
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time
@@ -27,12 +27,10 @@ le.fit(df['primary_use'])
 df['primary_use'] = le.transform(df['primary_use'])
 
 labels = df['meter_reading']
-print(df.shape)
 data = df.drop(columns=['meter_reading'])
 
 # Open to print results
-f = open('rf_results.txt','a+')
-print(len(data))
+f = open('nn_results.txt','a+')
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.3, random_state=42)
 
@@ -54,7 +52,7 @@ r2_dt = r2
 adj_r2_dt = adj_r2
 '''
 
-
+'''
 t = time.process_time()
 print("1")
 rf = RandomForestRegressor(n_estimators=25, criterion='friedman_mse')
@@ -73,20 +71,7 @@ r2 = r2_score(y_test, predictions, multioutput='variance_weighted')
 adj_r2 = 1 - ((1 - r2)*((len(y_test)-1)/(len(y_test)-len(X_test.columns) - 1)))
 r2_rf = r2
 adj_r2_rf =adj_r2
-
-
-# Note: returns negative values which causes error when calculating RSMLE
-'''linear_reg = LinearRegression(fit_intercept=False).fit(X_sub_train,y_sub_train)
-predictions = linear_reg.predict(X_valid)
-
-#rmsle_linear.append(np.sqrt(mean_squared_log_error(y_valid,predictions)))
-mae_linear.append(mean_absolute_error(y_valid, predictions))
-r2 = r2_score(y_valid, predictions, multioutput='variance_weighted')
-adj_r2 = 1 - ((1 - r2)*((len(y_valid)-1)/(len(y_valid)-len(X_valid.columns) - 1)))
-r2_linear.append(r2)
-adj_r2_linear.append(adj_r2)
-print('r2 ', r2)
-print('adj r2 ', adj_r2)'''
+'''
 
 
 # K-Nearest Neighbors (KNN) Results
@@ -107,7 +92,7 @@ adj_r2_knn = adj_r2
 '''
 
 
-'''
+
 t = time.process_time()
 nn = MLPRegressor(hidden_layer_sizes=(5,),activation='tanh',max_iter=1000).fit(X_train,y_train)
 elapsed_train = time.process_time() - t
@@ -122,7 +107,7 @@ r2 = r2_score(y_test, predictions, multioutput='variance_weighted')
 adj_r2 = 1 - ((1 - r2)*((len(y_test)-1)/(len(y_test)-len(X_test.columns) - 1)))
 r2_nn = r2
 adj_r2_nn = adj_r2
-'''
+
 
 '''
 print('training time: ', elapsed_train)
@@ -158,11 +143,6 @@ f.write('test Adj R^2 = '+ str(adj_r2_rf) +'\n')
 f.write('test MAE = ' + str(mae_rf) +'\n')
 '''
 
-'''print('test RMSLE = ', rslme_linear)
-print('test R^2 = ', r2_linear)
-print('test Adj R^2 = ', adj_r2_linear)
-print('test MAE = ', mae_linear)'''
-
 '''
 print('training time: ', elapsed_train)
 print('prediction time: ', elapsed_test)
@@ -180,7 +160,7 @@ f.write('test Adj R^2 = '+ str(adj_r2_knn) +'\n')
 f.write('test MAE = ' + str(mae_knn) +'\n')
 '''
 
-'''
+
 print('training time: ', elapsed_train)
 print('prediction time: ', elapsed_test)
 print('test RMSLE = ', rmsle_nn)
@@ -195,32 +175,9 @@ f.write('test RMSLE = ' + str(rmsle_nn) +'\n')
 f.write('test R^2 = ' + str(r2_nn) +'\n')
 f.write('test Adj R^2 = '+ str(adj_r2_nn) +'\n')
 f.write('test MAE = ' + str(mae_nn) +'\n')
-'''
+
 
 f.close()
-
-'''labels = pd.DataFrame(y_test, columns=["meter_reading"])
-preds = pd.DataFrame(predictions, columns=["meter_reading"])
-#labels["match"] = labels.eq(preds)
-#labels["diff"] = labels["meter_reading"] - preds["meter_reading"]
-
-print(len(labels))
-print(len(preds))
-labels["category"] = "true value"
-labels["idx"] = [i for i in range(0, len(labels))]
-preds["category"] = "predicted value"
-preds["idx"] = [i for i in range(0, len(preds))]
-labels_preds = labels.append(preds)'''
-
-#print(labels_preds_match)
-
-'''sns.set(style="darkgrid")
-ax = sns.scatterplot(x="idx", y="diff", data=labels)#, hue="category", style="category")
-plt.show()'''
-
-print(predictions)
-print('*')
-print(y_test)
 
 '''sns.set(style="darkgrid")
 ax = sns.distplot(predictions)
@@ -243,14 +200,13 @@ plt.title("True Meter Readings")
 plt.show()
 
 
-'''
-visualizer = ResidualsPlot(rf)
+
+visualizer = ResidualsPlot(nn)
 visualizer.fit(X_train, y_train)  # Fit the training data to the visualizer
 visualizer.score(X_test, y_test)  # Evaluate the model on the test data
 visualizer.show()
 
-visualizer = PredictionError(rf)
+visualizer = PredictionError(nn)
 visualizer.fit(X_train, y_train)  # Fit the training data to the visualizer
 visualizer.score(X_test, y_test)  # Evaluate the model on the test data
 visualizer.show()
-'''
